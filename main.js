93330dbd8c2f2f50c712791fe47aa4fd40063365
DeenoDev <deyanoklaasen@outlook.com>
2023-09-08 00:24:53 +0200

    Added responsive settings to javascript file.

--- main.js
+++ main.js
@@ -3,5 +3,20 @@ const slider = new A11YSlider(document.querySelector('.slider'), {
   dots: false,
   infinite: true,
   autoplay: true,
-  autoplaySpeed: 1000
+  autoplaySpeed: 1200,
+  autoplayHoverPause: true,
+  // responsive: {
+  //   768: {
+  //     slidesToShow: 2,
+  //     arrows: false
+  //   },
+  //   960: {
+  //     disable: true // slider disabled 960px to 1279px
+  //   },
+  //   1280: {
+  //     disable: false,
+  //     slidesToShow: 4,
+  //     dots: true // dots enabled 1280px and up
+  //   }
+  // }
 });
