17ced1b5581ee043a76e8e8de8e2777a738ae166
DeenoDev <deyanoklaasen@outlook.com>
2023-09-08 00:17:26 +0200

    Added autoplay option.

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const slider = new A11YSlider(document.querySelector('.slider'), {
   adaptiveHeight: true,
-  dots: false
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 1000
 });
