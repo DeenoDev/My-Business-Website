122082040b2aeab75dc6e4c2de54fd4e520a374c
DeenoDev <deyanoklaasen@outlook.com>
2023-09-08 22:16:05 +0200

    removed carousel arrows.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const slider = new A11YSlider(document.querySelector('.slider'), {
   adaptiveHeight: true,
   dots: false,
+  arrows: false,
   infinite: true,
   autoplay: true,
   autoplaySpeed: 1200,
