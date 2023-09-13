f90823a6bf9fa00adc46cbce9b6852eeb1c3e567
DeenoDev <deyanoklaasen@outlook.com>
2023-09-14 00:11:54 +0200

    Stopped sidescrolling on mobile version.

--- menu.js
+++ menu.js
@@ -1,11 +1,6 @@
 const toggle = document.querySelector('.toggle');
 const navigation = document.querySelector('.mobile-navigation');
 
-// toggle.addEventListener('click', () => {
-//     toggle.classList.toggle('active');
-//     navigation.classList.toggle('active');
-// });
-
 function toggleMenu(){
     toggle.classList.toggle('active');
     navigation.classList.toggle('active');
