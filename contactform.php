1b1c47214004beba0bf1c6240f0ff39775c88655
DeenoDev <deyanoklaasen@outlook.com>
2023-10-21 23:58:49 +0200

    Added mailTo and headers variables to contactform.php.

--- contactform.php
+++ contactform.php
@@ -4,4 +4,10 @@ if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
+
+    $mailTo ="deyanoklaasen@outlook.com";
+    $headers = "From: ".$mailFrom;
+
+
+    mail();
 }
